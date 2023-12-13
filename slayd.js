0fbc744dde5e9f0814def982b0f98d5bd5ea249a
unknown <petrov_vd_4311@mail.ru>
2023-12-14 01:13:03 +0300

    576-767 начал, нужно закончить футер

--- slayd.js
+++ slayd.js
@@ -6,12 +6,14 @@ function showSlides() {
   
 var element = document.querySelector('.fone');
 var element1 = document.querySelector('.fone1');
+var element2 = document.querySelector('.fone2');
 
 // Получение значения свойства display
 var displayValue = window.getComputedStyle(element).getPropertyValue('display');
 var displayValue1 = window.getComputedStyle(element1).getPropertyValue('display');
+var displayValue2 = window.getComputedStyle(element2).getPropertyValue('display');
 
-console.log(displayValue + displayValue1)
+
 
   if(displayValue=='block')
   {
@@ -49,4 +51,25 @@ console.log(displayValue + displayValue1)
 
 }
 
+
+if(displayValue2 =='block'){
+
+  var slides = document.getElementsByClassName("mySlides2");
+  var dots = document.getElementsByClassName("dot");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}    
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+  setTimeout(showSlides, 4000); // Меняйте изображение каждые 4 секунды
+
 }
+
+
+}
+
