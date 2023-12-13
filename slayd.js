13ccfc7d1f2c4ad512b9009c878245a20a15f86f
unknown <petrov_vd_4311@mail.ru>
2023-12-13 19:33:08 +0300

    768-991px и 992-1199px

--- slayd.js
+++ slayd.js
@@ -3,6 +3,19 @@ showSlides();
 
 function showSlides() {
   var i;
+  
+var element = document.querySelector('.fone');
+var element1 = document.querySelector('.fone1');
+
+// Получение значения свойства display
+var displayValue = window.getComputedStyle(element).getPropertyValue('display');
+var displayValue1 = window.getComputedStyle(element1).getPropertyValue('display');
+
+console.log(displayValue + displayValue1)
+
+  if(displayValue=='block')
+  {
+
   var slides = document.getElementsByClassName("mySlides");
   var dots = document.getElementsByClassName("dot");
   for (i = 0; i < slides.length; i++) {
@@ -16,4 +29,24 @@ function showSlides() {
   slides[slideIndex-1].style.display = "block";  
   dots[slideIndex-1].className += " active";
   setTimeout(showSlides, 4000); // Меняйте изображение каждые 4 секунды
+}
+
+  if(displayValue1 =='block'){
+
+  var slides = document.getElementsByClassName("mySlides1");
+  var dots = document.getElementsByClassName("dot");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}    
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+  setTimeout(showSlides, 4000); // Меняйте изображение каждые 4 секунды
+
+}
+
 }
