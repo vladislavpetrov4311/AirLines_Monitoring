b3331b36f59acbabc920b3157102253e09c02450
Vladislav4311 <petrov_vd_4311@mail.ru>
2024-03-11 05:30:27 +0300

    flask 7

--- flask/1.py
+++ flask/1.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, flash
+from flask import Flask, render_template, request, redirect, flash, session, url_for, abort
 
 app = Flask(__name__)
 
@@ -7,20 +7,34 @@ app = Flask(__name__)
 def form():
     return render_template('form.html')
 
+@app.route('/log')
+def log():
+    return render_template('login.html')
+@app.errorhandler(404)
+def pageNotFount(error):
+ return render_template('page404.html', title="Страница не найдена")
+
+
+@app.route("/")
+def index():
+ return render_template("index.html")
+
+
+
 
 @app.route('/login', methods=['POST', 'GET'])
 def login():
     if request.method == 'GET':
         return "Login via the login Form"
-
     if request.method == 'POST':
         password = request.form['password']
         if password == '123':
-            # The following flash message will be displayed on successful login
             flash('Login successful')
-            return render_template('success.html')
+            return render_template('Success.html')
         else:
-            return redirect('/form')
+            return redirect(url_for('log'))
+            #abort(401) #некст пример
+
 
 app.secret_key = 'secret_key'
 if __name__ == "__main__":
