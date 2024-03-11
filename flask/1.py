c765089cae3b7c38fc1f1b34c83bce473d4872fd
Vladislav4311 <petrov_vd_4311@mail.ru>
2024-03-11 04:33:51 +0300

    flask 6

--- flask/1.py
+++ flask/1.py
@@ -1,18 +1,27 @@
-from flask import Flask, redirect, url_for, request
+from flask import Flask, render_template, request, redirect, flash
+
 app = Flask(__name__)
 
-@app.route('/success/<name>')
-def success(name):
-   return 'welcome %s' % name
 
-@app.route('/login',methods = ['POST', 'GET'])
+@app.route('/form')
+def form():
+    return render_template('form.html')
+
+
+@app.route('/login', methods=['POST', 'GET'])
 def login():
-   if request.method == 'POST':
-      user = request.form['nm']
-      return redirect(url_for('success',name = user))
-   else:
-      user = request.args.get('nm')
-      return redirect(url_for('success',name = user))
+    if request.method == 'GET':
+        return "Login via the login Form"
 
-if __name__ == '__main__':
-   app.run(debug = True)
+    if request.method == 'POST':
+        password = request.form['password']
+        if password == '123':
+            # The following flash message will be displayed on successful login
+            flash('Login successful')
+            return render_template('success.html')
+        else:
+            return redirect('/form')
+
+app.secret_key = 'secret_key'
+if __name__ == "__main__":
+    app.run(debug=True)
