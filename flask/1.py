4ffca17b46ca14bf7a995d87d52b6c1f7cc24657
Vladislav4311 <petrov_vd_4311@mail.ru>
2024-03-11 03:31:38 +0300

    flask 4

--- flask/1.py
+++ flask/1.py
@@ -1,19 +1,25 @@
 from flask import Flask
 from flask import render_template
+from flask import  url_for
+
 app = Flask(__name__)
 
+menu = ["Установка", "Первое приложение", "Обратная связь"]
 @app.route("/")
 def index():
- return render_template("index.html")
-
-@app.route("/about")
-def about():
- return "<h1>О сайте</h1>"
-
-menu = ["Установка", "Первое приложение", "Обратная связь"]
+ return render_template('index.html', menu = menu)
 @app.route("/base")
 def base():
- return render_template('base.html', title="BASE", menu=menu)
+ return render_template('base.html', title = "О сайте", menu = menu)
+
+@app.route("/profile/<username>")
+def profile(username):
+ return f"Пользователь: {username}"
+
+ with app.test_request_context():
+  print(url_for('index'))
+  print(url_for('base'))
+  print(url_for('profile', username="selfedu"))
 
 if __name__ == "__main__":
  app.run(debug=True)
