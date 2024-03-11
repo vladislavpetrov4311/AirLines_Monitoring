efab9d6c2fece6faece15ed1f9051c550f45002a
Vladislav4311 <petrov_vd_4311@mail.ru>
2024-03-11 03:23:09 +0300

    flask 2

--- flask/1.py
+++ flask/1.py
@@ -1,14 +1,19 @@
 from flask import Flask
+from flask import render_template
 app = Flask(__name__)
 
 @app.route("/")
 def index():
- return "index"
+ return render_template("index.html")
 
 @app.route("/about")
 def about():
  return "<h1>О сайте</h1>"
 
+menu = ["Установка", "Первое приложение", "Обратная связь"]
+@app.route("/base")
+def base():
+ return render_template('base.html', title="BASE", menu=menu)
 
 if __name__ == "__main__":
  app.run(debug=True)
